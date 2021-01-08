0c4d1e8620075c18640b6f04699c6a2e4b76515a
pelsinkaplan <pelsinkaplan28@hotmail.com>
2021-01-08 23:31:31 +0300

    Add project files.

---
+++ AsalSayilar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsalSayilar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kacAsalSayiVar = 0;
+            for (int sayi = 2; sayi < 10000; sayi++)
+            {
+                bool asalMi = true;
+                for (int i = 2; i < sayi; i++)
+                    if (sayi % i == 0)
+                    {
+                        asalMi = false;
+                        break;
+                    }
+                if (asalMi == true)
+                {
+                    Console.Write(sayi + " ");
+                    kacAsalSayiVar++;
+                }
+            }
+            Console.WriteLine("\n2 ile 10000 arasında " + kacAsalSayiVar + " tane asal sayı var.");
+            Console.ReadKey();
+        }
+    }
+}
